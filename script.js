0cb8eefea0828ec68d9a9ae39123da18fc7e3862
mxrislyna <160684475+mxrislyna@users.noreply.github.com>
2026-08-14 19:16:34 -0500

    Add operator functions and function operate

---
+++ script.js
@@ -0,0 +1,32 @@
+//Operator functions
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+//Function operate
+function operate(a, operator, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default: 
+      return "ERROR";
+  } 
+}
